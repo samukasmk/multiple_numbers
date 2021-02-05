08c2d8c25e1e771f46436c747db7645fc7328333
Samuel Sampaio <samuel.sampaio@skyone.solutions>
2021-02-05 02:09:04 -0300

    [validation-functions] Declaring a external install requires num2words to get literal words of numbers

--- setup.py
+++ setup.py
@@ -20,5 +20,6 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
-    scripts=['scripts/detects-multiplicable-numbers'],
+    scripts=["scripts/detects-multiplicable-numbers"],
+    install_requires=["num2words>=0.5.10"],
 )
