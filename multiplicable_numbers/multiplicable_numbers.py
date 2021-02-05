9b45d9695c4fd39e9c1b276f84a1f9a8d5655685
Samuel Sampaio <samuel.sampaio@skyone.solutions>
2021-02-05 01:15:05 -0300

    [validation-functions] is_multiplicable function with check rule

--- multiplicable_numbers/multiplicable_numbers.py
+++ multiplicable_numbers/multiplicable_numbers.py
@@ -1,6 +1,16 @@
 def is_multiplicable(number, multiplicable):
-    pass
+    """The is_multiplicable function tells if a number is multiplicable by another
+
+       Args:
+           number (int): The number to compare.
+           multiplicable (int): The multiplicable number to check.
+
+       Returns:
+           bool: The return value. True if multiplicable, False otherwise.
+    """
+    return number % multiplicable == 0
+
 
 def detects_multiplicable_numbers(initial_number, final_number, multiplicables):
-    for number in range(initial_number, final_number+1):
+    for number in range(initial_number, final_number + 1):
         print(number)
