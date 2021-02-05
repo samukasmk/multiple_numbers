ff801d4be9784fc2b6f90778b302bb7e1e77a725
Samuel Sampaio <samuel.sampaio@skyone.solutions>
2021-02-05 00:04:09 -0300

    [project-stucture] Execution script detects-multiplicable-numbers.py

--- multiple_numbers/__init__.py
+++ multiple_numbers/__init__.py
@@ -1,7 +1,7 @@
-"""Multiple numbers module configuration."""
+"""Detects multiplicable numbers."""
+from .multiplicable_numbers import *
+
 __version__ = "0.0.1"
 __title__ = "multiple_numbers"
 __author__ = "Samuel Sampaio"
 __license__ = "Apache 2.0"
-
-print('oi')
--- setup.py
+++ setup.py
@@ -20,4 +20,5 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
+    scripts=['scripts/detects-multiplicable-numbers.py'],
 )
